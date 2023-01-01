6a5b733c58f2cbffd2e6399bc91607c96f8813bc
Jkhall81 <jason.kei.hall@gmail.com>
2023-01-01 13:57:06 -0700

    started hero component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero.jsx";
+
+function App() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <img
+        src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg"
+        alt="beach"
+      />
+    </div>
+  );
+};
+
+export default Hero;
