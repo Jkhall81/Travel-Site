0a5c9dbf1dd8f9010121e09c50ad44429ac840e9
Jkhall81 <jason.kei.hall@gmail.com>
2023-01-01 14:54:29 -0700

    starting navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Hero from "./components/Hero.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Hero />
     </div>
   );
